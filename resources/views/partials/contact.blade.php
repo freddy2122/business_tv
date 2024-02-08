192ff349b3f1885797f436d0516c9c6e04336dd5
freddy2122 <99898817+freddy2122@users.noreply.github.com>
2024-02-08 09:46:43 +0100

    Update contact.blade.php

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -36,15 +36,13 @@
             </div>
             <div class="col-lg-4 py-3">
                 <div class="display-4 text-center text-primary"><span class="mai-call"></span></div>
-                <p class="mb-3 font-weight-medium text-lg">Phone</p>
-                <p class="mb-0"><a href="#" class="text-secondary">+1 232 3235 324</a></p>
-                <p class="mb-0"><a href="#" class="text-secondary">+00 1122 3344 5566</a></p>
+                <p class="mb-3 font-weight-medium text-lg">Téléphone</p>
+                <p class="mb-0"><a href="https://wa.me/+22964897003?text=Salut nous voulons avoir plus de renseignement sur votre projet" class="text-secondary">+229 64897003</a></p>
             </div>
             <div class="col-lg-4 py-3">
                 <div class="display-4 text-center text-primary"><span class="mai-mail"></span></div>
                 <p class="mb-3 font-weight-medium text-lg">Email Address</p>
                 <p class="mb-0"><a href="#" class="text-secondary">support@seogram.com</a></p>
-                <p class="mb-0"><a href="#" class="text-secondary">hello@seogram.com</a></p>
             </div>
         </div>
     </div>
